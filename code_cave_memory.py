9a56ee5559e85abdf75deb99256b89682b159895
LuckyHacker <onni.hakkari@gmail.com>
2020-03-15 17:08:32 +0200

    Update code_cave_memory.py

--- code_cave_memory.py
+++ code_cave_memory.py
@@ -1,5 +1,7 @@
 import os
 import pickle
+import pefile
+import json
 
 from subprocess import Popen
 
@@ -25,24 +27,31 @@ class CodeCaveMemory:
     # Data access: CodeCaveMemory().data
     # Write data: CodeCaveMemory().write_data(data)
 
-    def __init__(self):
-        self.offsets_in_memory = False
+    def __init__(self, min_cave=128):
+        self.min_cave = min_cave
+
         self.exe_path = self.get_exe_path()
         self.exe_data, self.exe_data_string = self.read_exe(self.exe_path)
+        self.pe = pefile.PE(self.exe_path)
 
         self.init_memory()
+    
+    def init_memory(self):
+        # Init memory information
+        self.offsets_in_memory = False
+
+        self.offset_ranges = self.find_code_caves()
+        self.cave_sizes = list(map(lambda x: x[1] - x[0], self.offset_ranges))
+        self.data, self.data_len = self.read_data()
+        self.available_memory = sum(list(map(lambda x: x[1] - x[0], self.offset_ranges))) - self.data_len
+
+        if self.available_memory < 0:
+            raise Exception("MemoryError: CodeCaveMemory ran out of memory. Available memory: {}".format(
+                self.available_memory))
 
         if not self.offsets_in_memory:
             self.save_offsets()
             self.init_memory()
-
-        self.data, data_len = self.read_data()
-        self.available_memory = sum(list(map(lambda x: x[1] - x[0], self.offset_ranges))) - data_len
-    
-    def init_memory(self):
-        # Init memory information
-        self.offset_ranges = self.find_code_caves()
-        self.cave_sizes = list(map(lambda x: x[1] - x[0], self.offset_ranges))
 
     def get_exe_path(self):
         # Find out path to the binary itself
@@ -62,20 +71,29 @@ class CodeCaveMemory:
         if "OSstart" in self.exe_data_string and "OSend" in self.exe_data_string:
             offset_ranges = pickle.loads(bytes(self.exe_data_string.split("OSstart")[-1].split("OSend")[0], "latin-1"))
             self.offsets_in_memory = True
-        # If it is first run, find out available code caves
+        # If it is first run, find available code caves
         else:
             offset_ranges = []
+            fd = open(self.exe_path, "rb")
 
-            offset_range = []
-            for offset, byte in enumerate(self.exe_data):
-                if byte == 0 and len(offset_range) != 1:
-                    offset_range.append(offset+1)
+            for section in self.pe.sections:
+                if section.SizeOfRawData == 0:
+                    continue
 
-                if len(offset_range) == 1 and byte != 0:
-                    offset_range.append(offset-1)
-                    if offset_range[1] - offset_range[0] > 64:
-                        offset_ranges.append(offset_range)
-                    offset_range = []
+                pos = 0
+                count = 0
+                fd.seek(section.PointerToRawData, 0)
+                data = fd.read(section.SizeOfRawData)
+
+                for byte in data:
+                    pos += 1
+                    if byte == 0x00:
+                        count += 1
+                    else:
+                        if count > self.min_cave:
+                            raw_addr = section.PointerToRawData + pos - count - 1
+                            offset_ranges.append([raw_addr, raw_addr + count - 1])
+                        count = 0
 
         return offset_ranges
 
@@ -89,6 +107,8 @@ class CodeCaveMemory:
             self.exe_data[offset] = data[data_byte]
             data_byte += 1
 
+        self.exe_data_string = str(bytes(self.exe_data), "latin-1")
+
     def save_offsets(self):
         # Save offset addresses information to most suitable offset address
         offsets_data = b"OSstart" + pickle.dumps(self.offset_ranges) + b"OSend"
@@ -96,7 +116,9 @@ class CodeCaveMemory:
         cave_offset_idx = self.cave_sizes.index(offset_save_cave)
         cave_offset = self.offset_ranges[cave_offset_idx]
 
+        # Remove code cave that is used to store all other code cave information
         self.offset_ranges.pop(cave_offset_idx)
+
         offsets_data = b"OSstart" + pickle.dumps(self.offset_ranges) + b"OSend"
         self.write_at_offsets(cave_offset, offsets_data)
 
@@ -107,26 +129,32 @@ class CodeCaveMemory:
             for offset in range(offset_range[0], offset_range[1]+1):
                 data.append(self.exe_data[offset])
 
+            data.pop(-1) # For some reason last byte in code cave is always 0x00
+
+        data = str(bytes(data), "latin-1")
+        # Exclude offset information from the data
+        data_section1 = data.split("OSstart")[0]
+        data_section2 = data.split("OSend")[-1]
+        data = data_section1 + data_section2
+
         if len(list(set(data))) == 1:
             return (None, 0)
 
-        data = str(bytes(data), "latin-1")
         data = bytes(data.split("Dstart")[-1].split("Dend")[0], "latin-1")
-        data_len = len(data) + 10 # len(Dstart + Dend)
+        data_len = len(data.strip(b"\x00")) + 10 # len(Dstart + Dend)
         return (pickle.loads(data), data_len)
 
     def write_data(self, data):
         # Write data to available offset addresses
         # data : Python object
-        self.data = data
-
         storage_data = b"Dstart" + pickle.dumps(data) + b"Dend"
-
         data_start = 0
         for offset_range in self.offset_ranges:
             size = offset_range[1] - offset_range[0]
             self.write_at_offsets(offset_range, storage_data[data_start:data_start+size])
             data_start += size
+
+        self.init_memory()
 
 
 if __name__ == "__main__":
@@ -134,11 +162,15 @@ if __name__ == "__main__":
     CCM = CodeCaveMemory()
 
     print("{} bytes available in the CodeCaveMemory!".format(CCM.available_memory))
-    print("Data saved in the binary:", CCM.data)
+    print("Data saved in the binary:\n{}".format(json.dumps(CCM.data, indent=2)))
 
     user_message = input("Enter message: ")
     if len(user_message) != 0:
-        CCM.write_data({"message": user_message})
+        if CCM.data:
+            CCM.data["messages"].append(user_message)
+            CCM.write_data(CCM.data)
+        else:
+            CCM.write_data({"messages": [user_message]})
 
     # Always save current binary at the end
     save_exe_data(CCM.exe_path, CCM.exe_data)
